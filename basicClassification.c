c8e456f78a818c5ad4a10892dbe5f77a322b8c7d
MoranShalev <moranush10021@gmail.com>
2021-11-10 19:50:01 +0200

    fix isPrime

--- basicClassification.c
+++ basicClassification.c
@@ -2,35 +2,44 @@
 #include "NumClass.h"
 #include <math.h>
 
-
-
-int isPrime(int num){
-int i;
-for(i=2;i<=num/2;i++){
-if(num%i!=0){
-i++;}
-
-else return 0;
-}
-return 1;
-}
-
-int isStrong(int num){
-int sum=0;int azeret=1;int num2=num;int digit;
-while(num2>0)
+int isPrime(int num)
 {
-azeret=1;
-digit=num2%10;
-for( int i=digit;i>=1;i--){
-azeret=azeret*i;
-}
-sum=sum+azeret;
-num2=num2/10;
+    int i;
+    for (i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i != 0)
+        {
+            i++;
+        }
+
+        else
+            return 0;
+    }
+    return 1;
 }
 
-if (sum!=num){
-return 0;
-}
-else return 1;
-}
+int isStrong(int num)
+{
+    int sum = 0;
+    int azeret = 1;
+    int num2 = num;
+    int digit;
+    while (num2 > 0)
+    {
+        azeret = 1;
+        digit = num2 % 10;
+        for (int i = digit; i >= 1; i--)
+        {
+            azeret = azeret * i;
+        }
+        sum = sum + azeret;
+        num2 = num2 / 10;
+    }
 
+    if (sum != num)
+    {
+        return 0;
+    }
+    else
+        return 1;
+}
